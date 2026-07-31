576306d3cd78b330627cf64bf3a82722810b81bb
cheikhahmedtidiane <cisscheikhahmedtidiane31@gmail.com>
2026-07-31 16:08:43 +0000

    repot initial de l'atelier

---
+++ datasetManager/main.py
@@ -0,0 +1,6 @@
+# Application python permettant de gerer des jeux de données.
+
+nom_dataset = input("Entrez le nom du dataset : ")
+domain = input("Entrez le domaine du dataset : ")
+nb_lignes = int(input("Entrez le nombre de lignes du dataset : "))
+nb_colonnes = int(input("Entrez le nombre de colonnes du dataset : "))
